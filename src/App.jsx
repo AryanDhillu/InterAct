84119ff01478862b072d78f3b572b6313642cb90
AryanDhillu <dhilluaryan7@gmail.com>
2024-07-05 02:25:44 +0530

    d1 commit1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Chat from "./components/Chat/Chat"
+import Detail from "./components/Detail/Detail"
+import List from "./components/List/List"
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+      <List/>
+      <Chat/>
+      <Detail/>
+    </div>
   )
 }
 
